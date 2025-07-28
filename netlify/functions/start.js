563e6f03dd150ffc03c5b9502920464b68d42cda
FrankB4U <131400244+FrankB4U@users.noreply.github.com>
2025-07-28 15:58:50 -0700

    Create start.js

---
+++ netlify/functions/start.js
@@ -0,0 +1,21 @@
+exports.handler = async (event) => {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+
+  // GitHub OAuth endpoint
+  const githubAuthorizeUrl = "https://github.com/login/oauth/authorize";
+
+  // Redirect URI must match the callback function we will create next
+  const redirectUri = "https://agscms.netlify.app/.netlify/functions/auth/callback";
+
+  // Build authorization URL
+  const url = `${githubAuthorizeUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&scope=repo`;
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: url,
+    },
+  };
+};
