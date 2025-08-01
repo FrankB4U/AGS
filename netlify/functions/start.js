175b47bb69fa7a4492e3b18f2c4729bb0bcd6cb5
FrankB4U <131400244+FrankB4U@users.noreply.github.com>
2025-08-01 15:41:27 -0700

    Update start.js

--- netlify/functions/start.js
+++ netlify/functions/start.js
@@ -1,23 +1,13 @@
 exports.handler = async () => {
-  try {
-    // GitHub OAuth authorize URL
-    const authorizeUrl = new URL("https://github.com/login/oauth/authorize");
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const redirectUrl = "https://github.com/login/oauth/authorize";
 
-    authorizeUrl.searchParams.set("client_id", process.env.GITHUB_CLIENT_ID);
-    authorizeUrl.searchParams.set("redirect_uri", `${process.env.URL}/.netlify/functions/callback`);
-    authorizeUrl.searchParams.set("scope", "repo,user");
-    authorizeUrl.searchParams.set("allow_signup", "true");
+  const url = `${redirectUrl}?client_id=${clientId}&scope=public_repo,user`;
 
-    return {
-      statusCode: 302,
-      headers: {
-        Location: authorizeUrl.toString(),
-      },
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+  return {
+    statusCode: 302,
+    headers: {
+      Location: url,
+    },
+  };
 };
